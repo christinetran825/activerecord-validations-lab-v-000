bfcdebf13fd9a0e601ff47d66038d23e6fb82b6a
Christine Tran <christinetran825@gmail.com>
2017-11-13 05:14:28 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true, uniqueness: true
-  validates :content, presence: true, uniqueness: true
-  validates :summary, presence: true, uniqueness: true
-  validates :category, acceptance: true, uniqueness: true
+  # validates :content, presence: true, uniqueness: true
+  # validates :summary, presence: true, uniqueness: true
+  # validates :category, acceptance: true, uniqueness: true
 end
