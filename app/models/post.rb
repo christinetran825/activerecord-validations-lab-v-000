7fe7916dfc4be74194de864e402015ee00b8addd
Christine Tran <christinetran825@gmail.com>
2017-11-13 05:27:07 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 10 }
   validates :summary, length: {maximum: 10}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :is_clickbait?
 end
