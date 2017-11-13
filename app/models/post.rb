a397c803486946d42716725dd3f33d852193cf3b
Christine Tran <christinetran825@gmail.com>
2017-11-13 05:32:05 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,9 @@ class Post < ActiveRecord::Base
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
   validate :is_clickbait?
 
+  CLICKBAIT_LISTS = []"Won't Believe", "Won't Believe", "Secret", "Top", "Guess"]
+
+
   def is_clickbait?
     
   end
