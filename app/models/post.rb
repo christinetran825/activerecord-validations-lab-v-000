7ccd6d37d331478a7b1877818f6a08dfde4ad7b0
Christine Tran <christinetran825@gmail.com>
2017-11-13 05:17:16 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates :content, presence: true, uniqueness: true
+  validates :content, presence: true, length: { minimum: 10 }
   # validates :summary, presence: true, uniqueness: true
   validates :category, acceptance: true
 end
