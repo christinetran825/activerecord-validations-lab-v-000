c76d70c8c33b977b0c35e01b93d7c8900febca5c
Christine Tran <christinetran825@gmail.com>
2017-11-13 05:26:21 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates :content, presence: true, length: { minimum: 10 }
-  validates :summary, presence: true, length: {maximum: 10}
-  validates_inclusion_of :category
+  validates :content, length: { minimum: 10 }
+  validates :summary, length: {maximum: 10}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
 end
